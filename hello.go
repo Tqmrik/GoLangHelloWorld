cd80bae1c5b46f21489d127e2d188465d74fed61
Temk <steamxxlss@mail.ru>
2024-08-06 21:55:25 +0300

    Finale

--- hello.go
+++ hello.go
@@ -5,6 +5,5 @@ import (
 )
 
 func getSMSErrorString(cost float64, recipient string) string {
-	s := fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent", cost, recipient)
-	return s
+	return fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent", cost, recipient)
 }
