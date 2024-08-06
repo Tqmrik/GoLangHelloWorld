4441e6d6b413918faa417b2d4041d4206937da8f
Temk <steamxxlss@mail.ru>
2024-08-06 22:23:38 +0300

    Finished

--- hello_test.go
+++ hello_test.go
@@ -5,20 +5,22 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestDivide(t *testing.T) {
 	type testCase struct {
-		cost      float64
-		recipient string
-		expected  string
+		dividend, divisor, expected float64
+		expectedError               string
 	}
 	tests := []testCase{
-		{1.4, "+1 (435) 555 0923", "SMS that costs $1.40 to be sent to '+1 (435) 555 0923' can not be sent"},
-		{2.1, "+2 (702) 555 3452", "SMS that costs $2.10 to be sent to '+2 (702) 555 3452' can not be sent"},
+		{10, 2, 5, ""},
+		{15, 3, 5, ""},
 	}
 	if withSubmit {
 		tests = append(tests, []testCase{
-			{32.1, "+1 (801) 555 7456", "SMS that costs $32.10 to be sent to '+1 (801) 555 7456' can not be sent"},
-			{14.4, "+1 (234) 555 6545", "SMS that costs $14.40 to be sent to '+1 (234) 555 6545' can not be sent"},
+			{10, 0, 0, "can not divide 10 by zero"},
+			{15, 0, 0, "can not divide 15 by zero"},
+			{100, 10, 10, ""},
+			{16, 4, 4, ""},
+			{30, 6, 5, ""},
 		}...)
 	}
 
@@ -26,22 +28,26 @@ func Test(t *testing.T) {
 	failCount := 0
 
 	for _, test := range tests {
-		output := getSMSErrorString(test.cost, test.recipient)
-		if output != test.expected {
+		output, err := divide(test.dividend, test.divisor)
+		var errString string
+		if err != nil {
+			errString = err.Error()
+		}
+		if output != test.expected || errString != test.expectedError {
 			failCount++
 			t.Errorf(`---------------------------------
 Inputs:     (%v, %v)
-Expecting:  %v
-Actual:     %v
-Fail`, test.cost, test.recipient, test.expected, output)
+Expecting:  (%v, %v)
+Actual:     (%v, %v)
+Fail`, test.dividend, test.divisor, test.expected, test.expectedError, output, errString)
 		} else {
 			passCount++
 			fmt.Printf(`---------------------------------
 Inputs:     (%v, %v)
-Expecting:  %v
-Actual:     %v
+Expecting:  (%v, %v)
+Actual:     (%v, %v)
 Pass
-`, test.cost, test.recipient, test.expected, output)
+`, test.dividend, test.divisor, test.expected, test.expectedError, output, errString)
 		}
 	}
 
@@ -49,5 +55,6 @@ Pass
 	fmt.Printf("%d passed, %d failed\n", passCount, failCount)
 }
 
-// withSubmit is set at compile time depending on which button is used to run the tests
+// withSubmit is set at compile time depending
+// on which button is used to run the tests
 var withSubmit = true
